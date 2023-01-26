04174bd5b84e1a970d26782d690a1ef4cae03a98
S P JAGRIT <97080055+Spjagrit@users.noreply.github.com>
2023-01-26 15:44:06 +0530

    updated code

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+
+const Footer = () => {
+    return (
+        <div className="body">
+            <h1 className="footer">spjagrit's Foodary © 2023</h1>
+        </div>
+    );
+  }
+  export default Footer;
